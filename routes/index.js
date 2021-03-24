33c9dae6d594005dec183898f9cba55c0b6f8d7e
atemkumado <kumadnguyen123@gmail.com>
2021-03-24 22:24:27 +0700

    manage project for effective performance

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var app = require('../app');
+var expressLayouts = require('express-ejs-layouts');
+const { route } = require('./users');
+router.use(expressLayouts)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    res.render('index');
 });
 
 module.exports = router;
